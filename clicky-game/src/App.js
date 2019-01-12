c701749e2669172b8bef9a24404af333adde794b
Siberianeagle5 <christopherdavyydoconnell.com>
2019-01-12 15:59:10 -0500

    staged each section for the static pages

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Buttons from "./components/Buttons";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <div>
-            
+            <Navbar/>
+            <Jumbotron/>
+            <Buttons/>
+            <Footer/>
         </div>
     );
 };
---
+++ clicky-game/src/components/Buttons/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Buttons extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    };
+};
+
+export default Buttons;
